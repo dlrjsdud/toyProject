c56ef0da6f5efe83bf27fdbc95c08f930a6fe231
yuminji <raihns5@gmail.com>
2024-08-01 20:14:32 +0900

    8/1 유민지 네이버 로그인 중간 커밋

--- toyProject/java/egovframework/toy/cmmn/service/ToyLoginService.java
+++ toyProject/java/egovframework/toy/cmmn/service/ToyLoginService.java
@@ -1,6 +1,8 @@
 package egovframework.toy.cmmn.service;
 
+import java.io.UnsupportedEncodingException;
+
 public interface ToyLoginService {
 
-	public String getNaverAuthorizeUrl(String type);
+	public String getNaverAuthorizeUrl(String type) throws UnsupportedEncodingException;
 }
--- toyProject/java/egovframework/toy/cmmn/service/impl/ToyLoginServiceImpl.java
+++ toyProject/java/egovframework/toy/cmmn/service/impl/ToyLoginServiceImpl.java
@@ -1,31 +1,43 @@
 package egovframework.toy.cmmn.service.impl;
 
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import egovframework.toy.cmmn.service.NaverApi;
 import egovframework.toy.cmmn.service.ToyLoginService;
 
 @Service
 public class ToyLoginServiceImpl implements ToyLoginService {
 
-	public String getNaverAuthorizeUrl(String type){	
-		/*
-		 * String baseUrl = envConfig.getConfigValue("login.naver.baseUrl"); String
-		 * clientId = envConfig.getConfigValue("login.naver.clientId"); String
-		 * redirectUrl = envConfig.getConfigValue("login.naver.redirectUrl");
-		 * 
-		 * UriComponents uriComponents = UriComponentsBuilder .fromUriString(baseUrl +
-		 * "/" + type) .queryParam("response_type", "code") .queryParam("client_id",
-		 * clientId) .queryParam("redirect_uri", URLEncoder.encode(redirectUrl,
-		 * "UTF-8")) .queryParam("state", URLEncoder.encode("1234", "UTF-8")) .build();
-		 * 
-		 * return uriComponents.toString();
-		 */
-		return null;
+	@Override
+	public String getNaverAuthorizeUrl(String type) throws UnsupportedEncodingException{	
+		System.out.println("serviceimpl 들어옴");
+		System.out.println(type);
+		NaverApi naverApi;
+		
+		String baseUrl = "https://nid.naver.com/oauth2.0/";
+        //String clientId = naverApi.getNaverClientId();
+        String clientId = "Lytfg0QCqUNQaYbNPusV";
+        //String redirectUrl = naverApi.getNaverRedirectUri();
+        String redirectUrl = "http://localhost:8081/login/naver/code";
+        String clientSecret = "YpKL3v4wFp";
+        
+
+        UriComponents uriComponents = UriComponentsBuilder
+                .fromUriString(baseUrl + "/" + type)
+                .queryParam("response_type", "code")
+                .queryParam("client_id", clientId)
+                .queryParam("state", URLEncoder.encode("1234", "UTF-8"))
+                .queryParam("redirect_uri", URLEncoder.encode(redirectUrl, "UTF-8"))
+                .build();
+
+        System.out.println(uriComponents);
+        return uriComponents.toString();
 	}
 	
 }
