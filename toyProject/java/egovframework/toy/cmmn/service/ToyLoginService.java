43e77ba799264fb7425fb99ce16d9e965355e4f7
yuminji <raihns5@gmail.com>
2024-08-29 00:08:13 +0900

    8/29 유민지 네이버로그인 토근 커밋

--- toyProject/java/egovframework/toy/cmmn/service/ToyLoginService.java
+++ toyProject/java/egovframework/toy/cmmn/service/ToyLoginService.java
@@ -1,8 +1,16 @@
 package egovframework.toy.cmmn.service;
 
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ToyLoginService {
 
-	public String getNaverAuthorizeUrl(String type) throws UnsupportedEncodingException;
+	public String getNaverAuthorizeUrl(String type,HttpServletRequest request) throws UnsupportedEncodingException;
+	
+	public String getNaverTokenUrl(String type,HttpServletRequest request,@RequestParam("code")String code) throws MalformedURLException, UnsupportedEncodingException, URISyntaxException;
 }
--- toyProject/java/egovframework/toy/cmmn/web/LoginController.java
+++ toyProject/java/egovframework/toy/cmmn/web/LoginController.java
@@ -1,8 +1,10 @@
 package egovframework.toy.cmmn.web;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.security.SecureRandom;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import egovframework.toy.cmmn.service.ToyLoginService;
 
@@ -18,6 +21,8 @@ public class LoginController {
 	
 	@Autowired
 	ToyLoginService toyLoginService;
+	
+	
 	
 	@GetMapping("/login.do")
 	public String loginDo() {
@@ -28,8 +33,21 @@ public class LoginController {
 	@GetMapping("/login/naver-login.do")
 	public void naverLoging(HttpServletRequest request,HttpServletResponse response) throws MalformedURLException, UnsupportedEncodingException, URISyntaxException {
 		System.out.println("controller 들어옴");
-		String url = toyLoginService.getNaverAuthorizeUrl("authorize");
+		//String responseToken = toyLoginService.getNaverTokenUrl("authorize");
+		String url  = toyLoginService.getNaverAuthorizeUrl("authorize", request);
 		System.out.println(url);
+		try {
+            response.sendRedirect(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }		
+	}
+	
+	@GetMapping("/login/naver/code.do")
+	public void callBack(HttpServletRequest request, HttpServletResponse response,@RequestParam("code")String code) throws MalformedURLException, UnsupportedEncodingException, URISyntaxException{
+		System.out.println("콜백 컨드롤러 들어옴");
+		String responseToken = toyLoginService.getNaverTokenUrl("token", request,code);
+		System.out.println(responseToken);
 	}
 	
 	
